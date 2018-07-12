85794b7c299072ef002e79bb7f0d987902521d01
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-07-12 15:52:57 +0100

    Initial commit

---
+++ 4 - Getting Started with C# Programming Fundamentals/4.2 - Variables Demo/VariablesDemo/VariablesDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 21; //That's a lie
+            int x = 5;
+            double myHourlyRate = 150.00;
+            var myName = "Chris";
+            Console.WriteLine($"myAge: {myAge}, x: {x}, myHourlyRate: {myHourlyRate}, myName: {myName}");
+
+            myAge = 24;
+            x = 20;
+            myHourlyRate = 85.00;
+            myName = "Damian";
+            Console.WriteLine($"myAge: {myAge}, x: {x}, myHourlyRate: {myHourlyRate}, myName: {myName}");
+        }
+    }
+}
---
+++ 4 - Getting Started with C# Programming Fundamentals/4.8 - while Demo/whileDemo/whileDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whileDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            do
+            {
+                Console.WriteLine($"counter = {counter.ToString()}");
+                counter++;
+            } while (counter < 0) ;
+        }
+    }
+}
