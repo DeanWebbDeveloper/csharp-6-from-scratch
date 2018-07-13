4d16762bcd303c0a81a382a77f8c351868ad2958
DeanWebbDeveloper <dean@deanwebbdeveloper.com>
2018-07-13 15:22:48 +0100

    Chapters 6 - 10
    Chapters 6 - 10 completed

---
+++ 6 - Working with Classes/6.4 - Static Methods Demo/StaticDemo/StaticDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string selection = string.Empty;
+            while (selection != "q" && selection != "Q")
+            {
+                Console.WriteLine("Enter C)elsius to Fahrenheit or F)ahrenheit to Celsius or Q)uit");
+                selection = Console.ReadLine();
+                double fahrenheit = 0, celsius = 0;
+                switch(selection)
+                {
+                    case "C":
+                    case "c":
+                        Console.WriteLine("Please enter the Celsius temperature: ");
+                        fahrenheit = TemperatureConverter.CelsiusToFahrenheit(Console.ReadLine());
+                        Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit:f2}");
+                        break;
+
+                    case "F":
+                    case "f":
+                        Console.WriteLine("Please enter the Fahrenheit temperature: ");
+                        celsius = TemperatureConverter.FahrenheitToCelsius(Console.ReadLine());
+                        Console.WriteLine($"Temperature in Celsius: {celsius:f2}");
+                        break;
+
+                    case "Q":
+                    case "q":
+                        break;
+
+                    default:
+                        Console.WriteLine("Please try again");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 9 - Creating and Understanding Collections/9.5 - Demo Dictionary/ListDemo/ListDemo/Worker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo
+{
+    class Worker
+    {
+        public Worker()
+        {
+            Work();
+        }
+
+        public void Work()
+        {
+            Dictionary<string, Person> dict = new Dictionary<string, Person>();
+            Person george = new Person() { Name = "George", Age = 67 };
+            string key = "george";
+            dict.Add(key, george);
+
+            dict.Add("john", new Person() { Name = "John", Age = 44 });
+            dict.Add("thomas", new Person() { Name = "Thomas", Age = 80});
+            dict.Add("james", new Person() { Name = "James", Age = 61 });
+
+            Person secondPerson = dict["john"];
+            Console.WriteLine($"secondPerson = {secondPerson.Name}");        }
+    }
+}
